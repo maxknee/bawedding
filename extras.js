ad5129f8db6511fde33c6542c63d2fbb4b59acee
Max Knee <Max_Knee@comcast.com>
2017-04-22 18:00:09 -0400

    tidying up

--- extras.js
+++ extras.js
@@ -2,18 +2,21 @@
 const classList = document.getElementsByClassName('sec');
 window.onscroll = function() {
   var nav = document.getElementById('menu');
-  if ( window.pageYOffset > 100 ) {
+  if (window.pageYOffset > 100) {
     nav.classList.add('fixed');
+    document.getElementById('top-link').classList.remove('scroll-hide');
   } else {
     nav.classList.remove('fixed');
     [].forEach.call(classList, function(element) {
       element.classList.toggle('sec');
     });
+    document.getElementById('top-link').classList.add('scroll-hide');
   }
 };
 
 function toggleMobile(id) {
   let classList = document.getElementsByClassName('sec');
+
   if (classList.length) {
     [].forEach.call(classList, function(element) {
       element.classList.toggle('sec');
